a79b647bdd8f0dc649256a300bb636777d1079e4
jaganbaiju <jaganbaiju257@gmail.com>
2026-03-06 10:02:49 +0530

    hero section connected with backend api

--- src/components/heroPage.jsx
+++ src/components/heroPage.jsx
@@ -1,17 +1,37 @@
-import React from "react";
 import './heroPage.css';
 import { motion } from "framer-motion";
 import NavaBar from "./navBar";
 import HeroImg from "../assets/images/Hero-BG.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 function HeroPage() {
+    const BASE_URL = "http://127.0.0.1:8000";
+    const [heroData, setHeroData] = useState(null);
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/hero_page/")
+            .then((response) => response.json())
+            .then((data) => {
+                setHeroData(data.data);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    console.log(heroData);
+
+    // if (!heroData) {
+    //     return <p>Loading..</p>;
+    // }
+
     return (
         <div className="hero-page"
             style={{
-                backgroundImage: 
-                `linear-gradient(to bottom,transparent 60%,var(--background-color) 100%),
-                url(${HeroImg})`}}
+                backgroundImage:
+                    `linear-gradient(to bottom,transparent 60%,var(--background-color) 100%),
+                url(${BASE_URL}${heroData?.background_image})`
+            }}
         >
             <NavaBar />
             <div>
@@ -21,7 +41,8 @@ function HeroPage() {
                     transition={{ duration: 2, delay: 0.5 }}
                     viewport={{ once: true, amount: 0.5 }}
                 >
-                    PRINTING & BRANDING SOLUTION
+                    {/* PRINTING & BRANDING SOLUTION */}
+                    {heroData?.sub_heading}
                 </motion.h3>
                 <motion.h1 className="main-heading"
                     initial={{ scale: 0.6, opacity: 0 }}
@@ -29,10 +50,12 @@ function HeroPage() {
                     transition={{ duration: 2, delay: 0.5 }}
                     viewport={{ once: true, amount: 0.5 }}
                 >
-                    IN UAE
+                    {/* IN UAE */}
+                    {heroData?.main_heading}
                 </motion.h1>
                 <p className="description">
-                    We craft impactful Printing works , advertising , branding & digital marketing strategies to grow your brand and achieve measurable success!
+                    {/* We craft impactful Printing works , advertising , branding & digital marketing strategies to grow your brand and achieve measurable success! */}
+                    {heroData?.description}
                 </p>
             </div>
         </div >
