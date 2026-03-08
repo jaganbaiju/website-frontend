93e70a3e96a01f21337052dc797d7ac792b293bf
jaganbaiju <jaganbaiju257@gmail.com>
2026-03-08 12:50:52 +0530

    completed

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,12 +3,9 @@ import { Helmet } from "react-helmet";
 import "./dashboard.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function Dashboard() {
-
-  // const [title, setTitle] = useState("Hero Page");
-  // const [data, setData] = useState([]);
-  // const [heroData, setHeroData] = useState([]);
 
   return (
     <div className='dashboard'>
@@ -16,8 +13,18 @@ function Dashboard() {
         <title>Dashboard</title>
       </Helmet>
 
-      <h1>Dashboard</h1>
-
+      <nav>
+        <h1>Dashboard</h1>
+      </nav>
+      <hr />
+  
+      <a
+        href="https://website-backend-api-sz6i.onrender.com/admin/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open Admin Panel
+      </a>
     </div>
   );
 }
